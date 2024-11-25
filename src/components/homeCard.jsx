5ceb41368339580d9607f976941af544da3b1d1c
NurbekLDM <jumaboyevabarno80@gmail.com>
2024-11-25 11:29:47 +0500

    added searchQuery

--- src/components/homeCard.jsx
+++ src/components/homeCard.jsx
@@ -52,7 +52,7 @@ export default function HomeCard() {
                             <CardFooter
                                 className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                 <p className="text-tiny text-white/80">Avangers 3: Infinity war</p>
-                                <Link to="newfilms">
+                                <Link to="/newfilms">
                                 <Button  className="text-tiny text-white bg-black/20" variant="flat" color="default"
                                         radius="lg" size="sm">
                                    Watch Now
@@ -77,7 +77,7 @@ export default function HomeCard() {
                             <CardFooter
                                 className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                 <p className="text-tiny text-white/80">Joker</p>
-                                <Link to="newfilms">
+                                <Link to="/newfilms">
                                 <Button className="text-tiny text-white bg-black/20" variant="flat" color="default"
                                         radius="lg" size="sm">
                                     Watch Now
@@ -104,7 +104,7 @@ export default function HomeCard() {
                             <CardFooter
                                 className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                 <p className="text-tiny text-white/80">Avangers 3: Infinity war</p>
-                                <Link to="newfilms">
+                                <Link to="/newfilms">
                                 <Button className="text-tiny text-white bg-black/20" variant="flat" color="default"
                                         radius="lg" size="sm">
                                     Watch Now
@@ -128,7 +128,7 @@ export default function HomeCard() {
                             <CardFooter
                                 className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                 <p className="text-tiny text-white/80">Love and Monsters</p>
-                                <Link to="newfilms">
+                                <Link to="/newfilms">
                                 <Button className="text-tiny text-white bg-black/20" variant="flat" color="default"
                                         radius="lg" size="sm">
                                     Watch Now
